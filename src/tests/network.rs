a2ddc1bdc5717321874b6717a30eeb5224dc9efd
Henri Lunnikivi <henri.lunnikivi@gmail.com>
2018-02-19 19:40:29 +0200

    Remove timers from run_network -test

--- src/tests/network.rs
+++ src/tests/network.rs
@@ -18,43 +18,5 @@ fn run_network() -> ocl::Result<Vec<f32>> {
     );
     net.upload_buffers(&input_data, &queue).unwrap();
 
-    // TODO: replace with net.run() when phasing out of timers
-
-    let start_time = Instant::now();
-
-    // Enqueue the kernel for the 1st layer (Convolution + ReLU)
-    run_kernel_wait(&net.conv_relu1, &queue)?;
-    let conv1_done_time = Instant::now();
-
-    // Enqueue the kernel for the 2nd layer (Convolution + ReLU)
-    run_kernel_wait(&net.conv_relu2, &queue)?;
-    let conv2_done_time = Instant::now();
-
-    // Enqueue the 3rd layer (fully-connected)
-    run_kernel_wait(&net.dense3_kernel, &queue)?;
-    let dense3_out = relu(&unsafe { cl::read_buf(&net.dense3_out_buf)? });
-    let dense3_done_time = Instant::now();
-
-    // Run the 4th layer (fully-connected)
-    let dense4_out = mtxmul_relu(&dense3_out, &net.dense4);
-    let dense4_done_time = Instant::now();
-
-    // Run the 5th layer (fully-connected)
-    let output = mtxmul_softmax(&dense4_out, &net.dense5);
-    let end_time = Instant::now();
-
-    debug!(
-        "Per-layer computation time, L1: {}, L2: {}, L3: {}, L4: {}, L5: {}",
-        duration_between(start_time, conv1_done_time),
-        duration_between(conv1_done_time, conv2_done_time),
-        duration_between(conv2_done_time, dense3_done_time),
-        duration_between(dense3_done_time, dense4_done_time),
-        duration_between(dense4_done_time, end_time)
-    );
-    info!(
-        "Total computation time: {}",
-        duration_between(start_time, end_time)
-    );
-
-    Ok(output)
+    Ok(net.run(&queue))
 }

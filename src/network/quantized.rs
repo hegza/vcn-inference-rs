9f51a51c87e4f88c6ff3b2f943db6cbaabe6a723
Henri Lunnikivi <henri.lunnikivi@gmail.com>
2018-07-06 21:08:45 +0300

    Gate quantization tests

--- src/network/quantized.rs
+++ src/network/quantized.rs
@@ -7,10 +7,12 @@ mod test {
 
     const QUANTIZED_BASELINE: &'static str = SEPCONV_BASELINE;
 
-    fn run_quantized(input: &[u8]) -> Vec<f32> {
+    fn run_quantized(_input: &[u8]) -> Vec<f32> {
         Vec::new()
     }
 
+    // `cargo test --feature test_quantize` to run this test
+    #[cfg_attr(not(feature = "test_quantize"), ignore)]
     #[test]
     fn quantized_predicts() {
         let input = math::quantize_vec_u8(&f32::read_lines_from_file(&format!(

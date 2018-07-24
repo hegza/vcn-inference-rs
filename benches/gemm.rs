4695c5ea2392ada1e5ab61992c98b4d0fd3173c3
Henri Lunnikivi <henri.lunnikivi@gmail.com>
2018-07-24 11:06:22 +0300

    Rename bench sgemm -> gemm

---
+++ benches/gemm.rs
@@ -0,0 +1,349 @@
+#[macro_use]
+extern crate criterion;
+extern crate matrixmultiply;
+extern crate ndarray;
+extern crate num_traits;
+extern crate rand;
+extern crate rusty_cnn;
+
+use criterion::{Criterion, ParameterizedBenchmark, Throughput};
+use rand::Rng;
+use rusty_cnn::math::mtx_mul::gemm::*;
+use rusty_cnn::math::mtx_mul::gemm_naive;
+use rusty_cnn::verify;
+use std::collections::HashMap;
+
+const SAMPLE_SIZE: usize = 50;
+const NOISE_THRESHOLD: f64 = 0.06;
+const COARSE_RESULT_MARGIN: f32 = 0.0035f32;
+
+const D: usize = 64;
+
+/// On notation:
+/// host =  compiled Rust.
+/// GPU =   OpenCL / GPU.
+/// CPU =   OpenCL / CPU.
+///
+/// ## Test setup
+/// ### Initial setup
+/// An OpenCL kernel is compiled for each of the OpenCL benchmarks. One instance of the input
+/// matrices and the model output is allocated.
+/// Optionally: output is verified against the correct output.
+///
+/// ### Once before each Benchmark
+/// An instance of the input matrix is cloned using enclose! if necessary. The possible kernel and
+/// an instance of an output buffer are moved inside the closure.
+///
+/// ### Before each benchmark-thread
+/// Optionally: clone the inner input matrix with .clone().
+///
+/// ### After each Benchmark
+/// Cloned input matrices are dropped.
+///
+
+// Benchmark each layer separately.
+fn bench_gemm_variants(c: &mut Criterion) {
+    // Allocate input matrices and model output
+    let input_a = criterion::black_box(
+        String::from_utf8(include_bytes!("../src/tests/in/A_64x64.csv").to_vec())
+            .unwrap()
+            .split(',')
+            .map(|word| word.trim().parse::<f32>())
+            .filter_map(|res| res.ok())
+            .collect::<Vec<f32>>(),
+    );
+    let input_b = criterion::black_box(
+        String::from_utf8(include_bytes!("../src/tests/in/B_64x64.csv").to_vec())
+            .unwrap()
+            .split(',')
+            .map(|word| word.trim().parse::<f32>())
+            .filter_map(|res| res.ok())
+            .collect::<Vec<f32>>(),
+    );
+    let correct_c = String::from_utf8(include_bytes!("../src/tests/out/C_64x64.csv").to_vec())
+        .unwrap()
+        .split(',')
+        .map(|word| word.trim().parse::<f32>())
+        .filter_map(|res| res.ok())
+        .collect::<Vec<f32>>();
+
+    // TODO: bench upload + execute separately
+
+    const GROUP: &str = "gemm-f32";
+    //let input_sizes: Vec<usize> = vec![128, 64, 32];
+    let input_sizes: Vec<usize> = vec![128, 64];
+
+    let mut naive_out = vec![0f32; D * D];
+
+    // Verify result
+    gemm_naive(D, D, D, &input_a, &input_b, &mut naive_out);
+    verify(&naive_out, &correct_c, COARSE_RESULT_MARGIN);
+
+    // Create benchmark-closure
+    let bench = ParameterizedBenchmark::new(
+        "naive (host)",
+        move |be, &ds| {
+            // Measure
+            be.iter_with_setup(
+                || {
+                    (
+                        create_random_vec(ds * ds),
+                        create_random_vec(ds * ds),
+                        vec![0f32; ds * ds],
+                    )
+                },
+                |(a, b, mut c)| gemm_naive(ds, ds, ds, &a, &b, &mut c),
+            )
+        },
+        input_sizes.clone(),
+    );
+
+    // matrixmultiply::gemm uses row major instead of column-major and verification would be
+    // troublesome. Calculation takes the same amount of time with the incorrect matrices, however.
+
+    // Create benchmark-closure
+    let bench = bench.with_function(
+        // This is the implementation used by ndarray
+        "bluss_matrixmultiply (host)",
+        move |be, &ds| {
+            be.iter_with_setup(
+                || {
+                    (
+                        create_random_vec(ds * ds),
+                        create_random_vec(ds * ds),
+                        vec![0f32; ds * ds],
+                    )
+                },
+                |(a, b, mut c)| unsafe {
+                    matrixmultiply::sgemm(
+                        ds,
+                        ds,
+                        ds,
+                        1f32,
+                        a.as_ptr(),
+                        1,
+                        1,
+                        b.as_ptr(),
+                        1,
+                        1,
+                        1f32,
+                        c.as_mut_ptr(),
+                        1,
+                        1,
+                    )
+                },
+            )
+        },
+    );
+
+    // Setup
+    let mut gemm_1_gpu_out = input_sizes
+        .iter()
+        .map(|&ds| (ds, vec![0f32; ds * ds]))
+        .collect::<HashMap<usize, Vec<f32>>>();
+    let gemm_1 = input_sizes
+        .iter()
+        .map(|&ds| {
+            (
+                ds,
+                Naive1GemmKernel::from_slices(
+                    ds,
+                    ds,
+                    ds,
+                    &create_random_vec(ds * ds),
+                    &create_random_vec(ds * ds),
+                    &mut gemm_1_gpu_out.get_mut(&ds).unwrap(),
+                    DeviceType::ALL,
+                ),
+            )
+        })
+        .collect::<HashMap<usize, Naive1GemmKernel>>();
+
+    // Verify result by setting the known matrices
+    gemm_1[&D].set_buffers_from_slices(&input_a, &input_b);
+    gemm_1[&D].calculate_wait();
+    verify(&gemm_1_gpu_out[&D], &correct_c, COARSE_RESULT_MARGIN);
+
+    // Create benchmark-closure
+    let bench = bench.with_function("cnugteren_1_naive (GPU)", move |be, &ds| {
+        be.iter_with_setup(
+            || {
+                let (a, b) = (create_random_vec(ds * ds), create_random_vec(ds * ds));
+                gemm_1[&ds].set_buffers_from_slices(&a, &b);
+            },
+            |()| gemm_1[&ds].calculate_wait(),
+        )
+    });
+
+    //  Setup
+    let mut gemm_4_gpu_out = input_sizes
+        .iter()
+        .map(|&ds| (ds, vec![0f32; ds * ds]))
+        .collect::<HashMap<usize, Vec<f32>>>();
+    let gemm_4 = input_sizes
+        .iter()
+        .map(|&ds| {
+            (
+                ds,
+                Vectors4GemmKernel::from_slices(
+                    ds,
+                    ds,
+                    ds,
+                    &input_a,
+                    &input_b,
+                    &mut gemm_4_gpu_out.get_mut(&ds).unwrap(),
+                    DeviceType::ALL,
+                ),
+            )
+        })
+        .collect::<HashMap<usize, Vectors4GemmKernel>>();
+
+    // Verify Result
+    gemm_4[&D].calculate_wait();
+    verify(&gemm_4_gpu_out[&D], &correct_c, COARSE_RESULT_MARGIN);
+
+    // Create benchmark-closure
+    let bench = bench.with_function("cnugteren_4_vectors (GPU)", move |be, &ds| {
+        be.iter_with_setup(
+            || {
+                let (a, b) = (create_random_vec(ds * ds), create_random_vec(ds * ds));
+                gemm_4[&ds].set_buffers_from_slices(&a, &b);
+            },
+            |_| gemm_4[&ds].calculate_wait(),
+        )
+    });
+
+    // Setup
+    let mut gemm_5_gpu_out = input_sizes
+        .iter()
+        .map(|&ds| (ds, vec![0f32; ds * ds]))
+        .collect::<HashMap<usize, Vec<f32>>>();
+    let gemm_5 = input_sizes
+        .iter()
+        .map(|&ds| {
+            (
+                ds,
+                Transpose5GemmKernel::from_slices(
+                    ds,
+                    ds,
+                    ds,
+                    &input_a,
+                    &input_b,
+                    &mut gemm_5_gpu_out.get_mut(&ds).unwrap(),
+                    DeviceType::ALL,
+                ),
+            )
+        })
+        .collect::<HashMap<usize, Transpose5GemmKernel>>();
+
+    // Verify Result
+    gemm_5[&D].calculate_wait();
+    verify(&gemm_5_gpu_out[&D], &correct_c, COARSE_RESULT_MARGIN);
+
+    // Create benchmark-closure
+    let bench = bench.with_function("cnugteren_5_transpose (GPU)", move |be, &ds| {
+        be.iter_with_setup(
+            || {
+                let (a, b) = (create_random_vec(ds * ds), create_random_vec(ds * ds));
+                gemm_5[&ds].set_buffers_from_slices(&a, &b);
+            },
+            |_| gemm_5[&ds].calculate_wait(),
+        )
+    });
+
+    // Setup
+    let mut gemm_6_cpu_out = input_sizes
+        .iter()
+        .map(|&ds| (ds, vec![0f32; ds]))
+        .collect::<HashMap<usize, Vec<f32>>>();
+    let gemm_6_cpu = input_sizes
+        .iter()
+        .map(|&ds| {
+            (
+                ds,
+                Tiling6GemmKernel::from_slices(
+                    ds,
+                    ds,
+                    ds,
+                    &input_a,
+                    &input_b,
+                    &mut gemm_6_cpu_out.get_mut(&ds).unwrap(),
+                    DeviceType::CPU,
+                ),
+            )
+        })
+        .collect::<HashMap<usize, Tiling6GemmKernel>>();
+
+    // Verify Result
+    gemm_6_cpu[&D].set_buffers_from_slices(&input_a, &input_b);
+    gemm_6_cpu[&D].calculate_wait();
+    verify(&gemm_6_cpu_out[&D], &correct_c, COARSE_RESULT_MARGIN);
+
+    // Create benchmark-closure
+    let bench = bench.with_function("cnugteren_6_tiling (CPU)", move |be, &ds| {
+        be.iter_with_setup(
+            || {
+                // TODO: interface bug for CPU: can't write new values basically
+            },
+            |_| gemm_6_cpu[&ds].calculate_wait(),
+        )
+    });
+
+    // Setup
+    let mut gemm_6_gpu_out = input_sizes
+        .iter()
+        .map(|&ds| (ds, vec![0f32; ds]))
+        .collect::<HashMap<usize, Vec<f32>>>();
+    let gemm_6_gpu = input_sizes
+        .iter()
+        .map(|&ds| {
+            (
+                ds,
+                Tiling6GemmKernel::from_slices(
+                    ds,
+                    ds,
+                    ds,
+                    &input_a,
+                    &input_b,
+                    &mut gemm_6_gpu_out.get_mut(&ds).unwrap(),
+                    DeviceType::GPU,
+                ),
+            )
+        })
+        .collect::<HashMap<usize, Tiling6GemmKernel>>();
+
+    // Verify Result
+    gemm_6_gpu[&D].set_buffers_from_slices(&input_a, &input_b);
+    gemm_6_gpu[&D].calculate_wait();
+    verify(&gemm_6_gpu_out[&D], &correct_c, COARSE_RESULT_MARGIN);
+
+    // Create benchmark-closure
+    let bench = bench.with_function("cnugteren_6_tiling (GPU)", move |be, &ds| {
+        be.iter_with_setup(
+            || {
+                let (a, b) = (create_random_vec(ds * ds), create_random_vec(ds * ds));
+                gemm_6_gpu[&ds].set_buffers_from_slices(&a, &b);
+            },
+            |_| gemm_6_gpu[&ds].calculate_wait(),
+        )
+    });
+
+    c.bench(
+        GROUP,
+        bench.throughput(|&ds| {
+            Throughput::Bytes(std::mem::size_of::<f32>() as u32 * ds as u32 * ds as u32)
+        }),
+    );
+}
+
+fn create_random_vec(len: usize) -> Vec<f32> {
+    let mut rng = rand::thread_rng();
+    (0..len).map(|_| rng.gen_range(-1f32, 1f32)).collect()
+}
+
+criterion_group!{
+    name = benches;
+    config = Criterion::default().sample_size(SAMPLE_SIZE).noise_threshold(NOISE_THRESHOLD);
+    targets = bench_gemm_variants
+}
+criterion_main!(benches);

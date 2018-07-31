1417c6156b8b10bb617b0d9e79a206e7a5ca5c13
Henri Lunnikivi <henri.lunnikivi@gmail.com>
2018-07-31 20:09:19 +0300

    Optimize cnugteren_6 gemm

--- src/math/mtx_mul/gemm/tiling_6_gemm.rs
+++ src/math/mtx_mul/gemm/tiling_6_gemm.rs
@@ -24,22 +24,56 @@ impl OclGemm<Tiling6GemmKernel> for Tiling6GemmKernel {
         // Make sure enough space is reserved for the output buffer
         debug_assert_eq!(out.len(), m * n);
 
+        // Make sure the matrix dimensions are powers of two
+        debug_assert!((m & (m - 1)) == 0);
+        debug_assert!((n & (n - 1)) == 0);
+        debug_assert!((k & (k - 1)) == 0);
+
         // If Device uses RAM, use_host_ptr and mapping via address translation may be faster
         let use_host_ptr = device == DeviceType::CPU;
 
+        let (cache_line_size, max_lws) = if device == DeviceType::CPU {
+            (1, 1)
+        } else {
+            let device = cl_util::select_device(Some(device));
+            let cache_line_size = match device
+                .info(ocl::enums::DeviceInfo::GlobalMemCachelineSize)
+                .unwrap()
+            {
+                ocl::enums::DeviceInfoResult::GlobalMemCachelineSize(x) => x,
+                _ => panic!("ocl API returned incorrect result"),
+            };
+            let dev_max_lws = device.max_wg_size().unwrap();
+            (cache_line_size as usize, dev_max_lws)
+        };
+
+        // Optimal tile-size is as close to the preferred maximum work-group-size while still
+        // fitting into the max work group size on GPU. cnugteren used hard-coded 128x128.
+        let ts = min(cache_line_size, ((m * n) as f64).sqrt() as usize);
+
+        // TODO: the main performance reciprocate here seems to be the amount of local memory used by the kernel
+        // TODO: get local memory size (32768) on this device and fit the amount of memory used by the kernel into that
+
         // The tile-size in dimension m
-        let tsm: usize = 32;
+        let tsm: usize = ts;
         // The tile-size in dimension n
-        let tsn: usize = 32;
+        let tsn: usize = ts;
         // The tile-size in dimension k
         let tsk: usize = 16;
         // The amount of work-per-work-item in dimension m
-        let wpwim: usize = 8;
+        // NOTE: increasing these to 8 decreases the performance by 50 % and to 16 by around 1000 %
+        let wpwim: usize = 4;
         // The amount of work-per-work-item in dimension n
-        let wpwin: usize = 8;
+        let wpwin: usize = 4;
+
+        // Optimal tile-size is as close to the preferred maximum work-group-size while still
+        // fitting into the max work group size on GPU and 1 on CPU because no autovectorization is
+        // possible for this kernel. cnugteren used hard-coded 32x32.
+        let local_work_side = (max_lws as f64).sqrt() as usize;
+
         // Dimensions for local memory optimization
-        const TRANSPOSEX: usize = 16;
-        const TRANSPOSEY: usize = 16;
+        let transposex = local_work_side;
+        let transposey = local_work_side;
 
         let src_transpose = String::from_utf8_lossy(include_bytes!("cl/transpose.cl"));
         let src_mtx_mul = String::from_utf8_lossy(include_bytes!("cl/6_register_tiling.cl"));
@@ -53,8 +87,8 @@ impl OclGemm<Tiling6GemmKernel> for Tiling6GemmKernel {
                 &format!("-D TSK={}", tsk),
                 &format!("-D WPWIM={}", wpwim),
                 &format!("-D WPWIN={}", wpwin),
-                &format!("-D TRANSPOSEX={}", TRANSPOSEX),
-                &format!("-D TRANSPOSEY={}", TRANSPOSEY),
+                &format!("-D TRANSPOSEX={}", transposex),
+                &format!("-D TRANSPOSEY={}", transposey),
             ],
             Some(device),
         );
@@ -94,7 +128,7 @@ impl OclGemm<Tiling6GemmKernel> for Tiling6GemmKernel {
             .program(&program)
             .name("transpose")
             .queue(queue.clone())
-            .local_work_size(SpatialDims::Two(TRANSPOSEX, TRANSPOSEY))
+            .local_work_size(SpatialDims::Two(transposex, transposey))
             .global_work_size(SpatialDims::Two(k, n))
             .arg(k as i32)
             .arg(n as i32)
